de87ef2db6c24bd37c0c08bb65f008dfed987e3b
jerros740 <jeremy.ros@edu.esiee.fr>
2024-11-05 20:26:05 +0100

    Adding background in home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import {Header} from "./layout/header/header";
+import {HomePage} from "./pages/home-page/HomePage";
 
 function App() {
   return (
     <div className="app">
       <Header />
+        <HomePage />
     </div>
   );
 }
