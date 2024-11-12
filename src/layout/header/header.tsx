690731492fb0a15c47362d598feb26584aabf601
jerros740 <jeremy.ros@edu.esiee.fr>
2024-11-12 21:43:13 +0100

    Adding input text to look up for summoner name

--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, {ChangeEvent, ChangeEventHandler, useState} from 'react';
 import icon from '../../assets/icon.webp'
 
 export function Header() {
+    const [summonerName, setSummonerName] = useState("");
+
+    const onSummonerNameChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        setSummonerName(event.target.value);
+    }
+
+    const checkSummoner = (): Object => {
+        // TODO
+        return {};
+    }
+
     return (
         <div className="header">
             <div className="title">
                 <div className="title__text">TFT ZONE</div>
             </div>
             <div className="navigation">
-                <div className="navigation__button">Equipe</div>
-                <div className="navigation__button">Champions</div>
-                <div className="navigation__button">Objets</div>
-                <div className="navigation__button">Augment</div>
-                <div className="navigation__button">Charms</div>
-                <div className="navigation__button">Statistiques</div>
-                <div className="navigation__button">Joueurs</div>
-                <div className="navigation__button">Création d'équipe</div>
+                {/*<div className="navigation__button">Equipe</div>*/}
+                {/*<div className="navigation__button">Champions</div>*/}
+                {/*<div className="navigation__button">Objets</div>*/}
+                {/*<div className="navigation__button">Augment</div>*/}
+                {/*<div className="navigation__button">Charms</div>*/}
+                {/*<div className="navigation__button">Statistiques</div>*/}
+                {/*<div className="navigation__button">Joueurs</div>*/}
+                {/*<div className="navigation__button">Création d'équipe</div>*/}
+                <input
+                    type="text"
+                    value={summonerName}
+                    src={summonerName}
+                    onChange={onSummonerNameChange}
+                    onKeyDown={checkSummoner}
+                />
             </div>
         </div>
     );
