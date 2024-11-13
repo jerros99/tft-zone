a7dd11f0d487a45b0ab122a5214bca1fb9328d76
jerros740 <jeremy.ros@edu.esiee.fr>
2024-11-13 09:41:48 +0100

    Using config files in SummonerApi.ts

--- src/api/Summoner/SummonerApi.ts
+++ src/api/Summoner/SummonerApi.ts
@@ -1,11 +1,10 @@
 import {SummonerType} from "./SummonerType";
+import {SUMMONER_BASE_URL} from "./SummonerConfig";
+import {API_KEY} from "../ApiConfig";
 
-const API_KEY: string = "RGAPI-e04c4ab3-12ff-4f75-b59b-011dbb533a61";// To hide
-
-const BASE_URL: string = "https://europe.api.riotgames.com";
 
 export const fetchSummoner = (summonerName: string): void => {
-    fetch(`${BASE_URL}/riot/account/v1/accounts/by-riot-id/${summonerName}/EUW`,{
+    fetch(`${SUMMONER_BASE_URL}/riot/account/v1/accounts/by-riot-id/${summonerName}/EUW`, {
         method: 'GET',
         headers: {
             "X-Riot-Token": API_KEY
