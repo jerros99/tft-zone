9625d2fd09017d294b86daa7ee6dfca3cef5e3a5
jerros740 <jeremy.ros@edu.esiee.fr>
2024-11-14 18:00:42 +0100

    Loading message added with percentage

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,13 +1,18 @@
 import React, {useContext} from 'react';
 import bgImage from '../../assets/bg-tft-zone.jpg'
 import {TFTDataContext} from "../../App";
-import Loader from "../../component/loader/Loader";
+import {Loader} from "../../component/loader/Loader";
 
 type Props = {
     children: React.ReactNode,
 }
 export function HomePage({children}:Props) {
     const {isLoading, dataCounter, totalGameData} = useContext(TFTDataContext)
+
+    const getLoadingPercentage = () => {
+        return  Math.round((dataCounter/totalGameData)*100);
+    }
+
     return (
         <div className="home-page">
             <div className="home-page__background">
@@ -16,7 +21,7 @@ export function HomePage({children}:Props) {
 
             {isLoading && (
                 <span>
-                    <Loader />
+                    <Loader  loadingMessage={`Récupération des données en cours... ${getLoadingPercentage()}%`}/>
                     {`${dataCounter}/${totalGameData}`}
                 </span>
             )}
