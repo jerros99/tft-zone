68a455ccb18d5b0107bd497e5e1c9a474b5f51a2
jerros740 <jeremy.ros@edu.esiee.fr>
2024-11-14 17:53:49 +0100

    Fixing background to absolute and loader added to homepage

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import bgImage from '../../assets/bg-tft-zone.jpg'
+import {TFTDataContext} from "../../App";
+import Loader from "../../component/loader/Loader";
 
-export function HomePage() {
+type Props = {
+    children: React.ReactNode,
+}
+export function HomePage({children}:Props) {
+    const {isLoading, dataCounter, totalGameData} = useContext(TFTDataContext)
     return (
         <div className="home-page">
             <div className="home-page__background">
                 <img src={bgImage}/>
             </div>
+
+            {isLoading && (
+                <span>
+                    <Loader />
+                    {`${dataCounter}/${totalGameData}`}
+                </span>
+            )}
         </div>
     );
 }
