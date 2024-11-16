3ce20754e70a17a64cbb243710084e9fc9a2ae7f
jerros740 <jeremy.ros@edu.esiee.fr>
2024-11-16 12:09:04 +0100

    Removing unused code

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,21 +1,8 @@
-import React, {useContext} from 'react';
-import {TFTDataContext} from "../../App";
-import {Loader} from "../../component/loader/Loader";
+import React from 'react';
 
 export function HomePage() {
-    const {isLoading, dataCounter, totalGameData} = useContext(TFTDataContext)
-
-    const getLoadingPercentage = () => {
-        return Math.round((dataCounter / totalGameData) * 100);
-    }
-
     return (
         <>
-            {isLoading && (
-                <span>
-                    <Loader loadingMessage={`Récupération des données en cours... ${getLoadingPercentage()}%`}/>
-                </span>
-            )}
         </>
     );
 }
